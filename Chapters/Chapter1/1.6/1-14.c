ad93e4cd03fd7f742e26bbf0b4e29309d9bd3e5b
Richard Jiang <richardjiang6@gmil.com>
2023-08-19 16:08:13 -0400

    finally finished the 1-14.c program and now it not only can count digits but it can count letters as well

--- Chapters/Chapter1/1.6/1-14.c
+++ Chapters/Chapter1/1.6/1-14.c
@@ -4,29 +4,59 @@
 
 void histo(int *p, char *lp, int arraylen, int space, int width, char symbol);
 
-/* currently only reads integer digits. a heavily modified version of digithistogram.c */
+/* a heavily modified version of digithistogram.c */
 main()
 {
 	int c, i, tmp, nwhite, nother;
 	int ndigit[10];
-	char label[10];	/* yep, the array is a char array and the labels are stored as chars */
+	char digitlabel[10];	/* yep, the array is a char array and the labels are stored as chars */
+	int nupletter[26];
+	char upletterlabel[26];
+	int nlowletter[26];
+	char lowletterlabel[26];
+	int blank[2];	/* I think that my histo function only takes a pointer to an array is input unfortunately */
+	char blanklabel[2];
 
 	nwhite = nother = 0;
-	for (i = 0; i <= 9; ++i) {
+	for (i = 0; i < 10; ++i) {
 		ndigit[i] = 0;	/* makes every element of the array have a value of 0 */
-		label[i] = i + '0';	/* makes label[0] have char 0, label[1] have char 1, etc. */
+		digitlabel[i] = i + '0';	/* makes label[0] have char 0, label[1] have char 1, etc. */
+	}
+	for (i = 0; i < 26; ++i) {
+		nupletter[i] = 0;	/* makes every element of the array have a value of 0 */
+		upletterlabel[i] = i + 'A';	/* makes label[0] have char 0, label[1] have char 1, etc. */
+	}
+	for (i = 0; i < 26; ++i) {
+		nlowletter[i] = 0;	/* makes every element of the array have a value of 0 */
+		lowletterlabel[i] = i + 'a';	/* makes label[0] have char 0, label[1] have char 1, etc. */
 	}
 
-	while ((c = getchar())!= EOF)
+	while ((c = getchar())!= EOF) {
 		if (c >= '0' && c <= '9')
 			++ndigit[c-'0'];
+		else if (c >= 'A' && c <= 'Z')
+			++nupletter[c-'A'];
+		else if (c >= 'a' && c <= 'z')
+			++nlowletter[c-'a'];
 		else if (c == ' ' || c == '\n' || c == '\t')
 			++nwhite;
 		else
 			++nother;
+	}
+
+	blank[0] = nwhite;
+	blank[1] = nother;
+	blanklabel[0] = 'w';	/* unfortunately, the histo function label code can only print a single char for each label. w stands for 'white' */
+	blanklabel[1] = 'o';	/* o stands for 'other' */
 	
-	histo(ndigit, label, 10, 2, 4, '%');	/* it's very easy to modify the settings of the histogram */
+	histo(ndigit, digitlabel, 10, 2, 4, '%');	/* it's very easy to modify the settings of the histogram */
 	/* histo(heigh data, label data, array length, space between column, column width, column symbol) */
+
+	histo(nupletter, upletterlabel, 26, 2, 4, '%');
+
+	histo(nlowletter, lowletterlabel, 26, 2, 4, '%');
+
+	histo(blank, blanklabel, 2, 2, 4, '%');
 }
 
 
