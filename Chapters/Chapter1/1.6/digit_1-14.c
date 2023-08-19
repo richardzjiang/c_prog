1a16fefdb111de12b9efccb3244cb1f05bbbb092
Richard Jiang <richardjiang6@gmil.com>
2023-08-19 15:44:19 -0400

    fixed the blurb at the top of the program

--- Chapters/Chapter1/1.6/digit_1-14.c
+++ Chapters/Chapter1/1.6/digit_1-14.c
@@ -1,4 +1,4 @@
-/* Write a program to print a histogram of the length of words in its input. It is easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging. */
+/* Write a program to print a histogram of the frequencies of different characters in its input. */
 
 #include <stdio.h>
 
