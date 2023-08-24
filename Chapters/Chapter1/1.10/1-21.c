13bffe36338659a02ec8a61cb7ece2554aae936f
Richard Jiang <richardjiang6@gmil.com>
2023-08-24 11:12:39 -0400

    finished exercise 1-21

--- Chapters/Chapter1/1.10/1-21.c
+++ Chapters/Chapter1/1.10/1-21.c
@@ -7,33 +7,38 @@ main()
 {
 	int c;
 	int n;		/* how many chars read in line*/
-	int pos;	/* it is the length of the line read so far, it is distinct from n because tabs can have a length of more than one char */
+	int pos;	/* position. it is the length of the line read so far, it is distinct from n because tabs can have a length of more than one char */
 	int nsp;	/* number of spaces */
 
 	while ((c = getchar()) != EOF) {
-		if (c == '\n') {
+		if (c == '\n') {	/* it's a new line. everything restarts */
 			n = 0;
 			pos = 0;
-			nsp = 0;
+			for (;nsp > 0; --nsp)	/* this also conveniently makes nsp = 0 */
+				putchar(' ');
 			putchar(c);
 			continue;
 		}
 		if (c == '\t') {
 			++n;
-			if ((TABLEN - (pos % TABLEN)) == 0)
-				pos += TABLEN;
-			else
-				pos += (TABLEN - (pos % TABLEN));
+			pos += (TABLEN - (pos % TABLEN));
 			putchar(c);
 			continue;
 		}
-		if (c == ' ')
-			++nsp;	/* no need for putchar(c) here (the putchar(c) at the end of the loop will do it) */
-		if (((pos + nsp) % TABLEN) == 0) {	/* putting a tab will result in pos being moved to a tab column */
-			putchar('\t');
-			pos += nsp;
+		if (c == ' ') {
+			++nsp;
+			if (((pos + nsp) % TABLEN) == 0) {	/* putting a tab will result in pos being moved to a tab column */
+				putchar('\t');
+				pos += nsp;
+				nsp = 0;
+			}
+			//printf("DEBUG: nsp=%d\n", nsp);
 			continue;
 		}
+		/* below this point means that c is a char other than space, tab, or newline */
+		++pos;
+		for (;nsp > 0; --nsp)	/* this also conveniently makes nsp = 0. if c is a normal, non-white char, then nsp is zero */
+			putchar(' ');
 		putchar(c);
 	}
 }
