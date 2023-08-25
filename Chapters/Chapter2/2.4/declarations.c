db603804340b044da28a297320edecad095f633b
Richard Jiang <richardjiang6@gmil.com>
2023-08-25 15:41:27 -0400

    2.4 finished

---
+++ Chapters/Chapter2/2.4/declarations.c
@@ -0,0 +1,18 @@
+int lower, upper step;
+char c, line[1000];
+
+int lower;
+int upper;
+int step;
+char c;
+char line[1000];
+
+char esc = '\\';
+int i = 0;
+int limit = MAXLINE+1;
+float eps = 1.0e-5;
+
+const double e = 2.71828182845905;
+const char msg[] = "warning: ";
+
+int strlen(const char[]);
