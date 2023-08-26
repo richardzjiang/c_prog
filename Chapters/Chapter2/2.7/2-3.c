52b5f0e110ca4004259cfe624bd13f07823815ec
Richard Jiang <richardjiang6@gmil.com>
2023-08-26 19:37:27 -0400

    finished 2-3.c and added some extra functionality

--- Chapters/Chapter2/2.7/2-3.c
+++ Chapters/Chapter2/2.7/2-3.c
@@ -7,20 +7,40 @@ int htoi(char *s);
 
 main()
 {
-	int c, i;
+	int c;
+	int i;
+	int invalidchars;
 	char input[MAXINPUT];
 
+	i = 0;
+	invalidchars = 0;
+	printf("Convert hexadecimal input to decimal!\n");
+	printf("_____________________________________\n");
 	while (c = getchar()) {
-		if (c == '\n') {
+		if (c == EOF) {
+			input[i] = '\0';
 			printf("%d\n", htoi(input));
-			i = 0;
-		}
-		else if (c == EOF) {
-			printf("%d\n", htoi(input));
+			if (invalidchars)
+				printf("Contains invalid chars\n");
+			invalidchars = 0;
 			break;
 		}
-		else
-			input[i++] = c;
+		if (c == '\n') {
+			input[i] = '\0';
+			printf("%d\n", htoi(input));
+			if (invalidchars)
+				printf("Contains invalid chars\n");
+			invalidchars = 0;
+			i = 0;
+		} else if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')) {
+			if ((i + 1) < (MAXINPUT - 1))
+				input[i++] = c;
+			else {
+				printf("Input too long!\n");
+				break;
+			}
+		} else
+			invalidchars = 1;
 	}
 }
 
@@ -29,16 +49,13 @@ int htoi(char *s)
 	int i;
 	int output = 0;
 
-	if (i == 0)
-		return 0;
-
 	for (i = 0; s[i] != '\0'; ++i) {
 		if (s[i] >= '0' && s[i] <= '9')
-			output = output * 10 + (s[i] - '0');
+			output = output * 16 + (s[i] - '0');
 		if (s[i] >= 'a' && s[i] <= 'f')
-			output = output * 10 + (s[i] - 'a' + 10);
+			output = output * 16 + (s[i] - 'a' + 10);
 		if (s[i] >= 'A' && s[i] <= 'F')
-			output = output * 10 + (s[i] - 'A' + 10);
+			output = output * 16 + (s[i] - 'A' + 10);
 	}
 	return output;
 }
