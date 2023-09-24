6048d9f118be187d3ec0f401c6975171d6aaeb2e
Richard Jiang <richardjiang6@gmil.com>
2023-09-23 21:44:55 -0400

    it turns out that 4-5.c still needs some work

--- Chapters/Chapter4/4.3/4-5.c
+++ Chapters/Chapter4/4.3/4-5.c
@@ -57,6 +57,8 @@ main()
 			else
 				printf("undefined: zero to the power of zero\n");
 			break;
+		case -1:
+			break;	/* do absolutely nothing (don't print an error message) */
 		case 0:
 			push(sin(dtor((float) pop())));
 			break;
@@ -64,6 +66,7 @@ main()
 			push(exp(pop()));
 			break;
 		case '\n':
+			printf("DEBUG: printed result\n");
 			printf("\t%.8g\n", pop());
 			break;
 		default:
@@ -107,6 +110,8 @@ double pop(void)
 
 #include <ctype.h>
 
+#define MAXWORD 20
+
 int getch(void);
 void ungetch(int);
 
@@ -114,11 +119,12 @@ void ungetch(int);
 int getop(char s[])
 {
 	int i, c;
-	char test[3]; /* sin, exp, and pow only have three letters. thus, the number here is three because only three elements are needed */
+	char test[MAXWORD];
 	int n;
 
 	while ((s[0] = c = getch()) == ' ' || c == '\t')
 		;
+	printf("DEBUG: c = %c\n", c);
 	s[1] = '\0';
 	if (!isdigit(c) && !isalpha(c) &&c != '.')
 		return c;	/* not a number (or letter) */
@@ -127,29 +133,47 @@ int getop(char s[])
 		while (isdigit(s[++i] = c = getch()))	/* while loop runs until getch() is no longer a digit */
 			;
 	n = 0;	/* n actually points to the last occupied slot of test, not the next empty one */
-	if (isalpha(c)) {	/* identified error: the 'word' that is collected by test[] is shifted right by one char */
+	if (isalpha(c)) {
 		test[n++] = c;	/* without this, c's value will be erased by 'c = getch()' */
 		while (isalpha(test[n++] = c = getch())) {
-			if (n > 2)	/* the group of letters exceeds three characters. therefore, it cannot be sin, exp or pow */
-				break;
+			if ((n + 1) > MAXWORD) {
+				printf("error: exceeds maximum word size\n");
+				return -1;	/* returning -1 basically means to do nothing, not even print an error message */
+			}
+			if (test[n--] == '\n') {
+				ungetch(c);	/* puts test[n] (which is the previous c) into buf[]. next time the getop function is used, c will receive this '\n' instead of a new char from the input */
+				printf("DEBUG: ungetched\n");
+				return -1;
+			}
+				
+			//if (n > 2)
+			//	break;
 		}
 		test[n] = '\0';
-		if (n < 2 || n > 3) {
-			for (n = 0; test[n] != '\0'; ++n)
-				printf("error: n < 2 || n > 3: unknown command %c\n", test[n]);
-			return c;
+		//if (test[n-1] == '\n')
+		//	test[--n] = '\0';
+		printf("DEBUG: n = %d\n", n);
+		printf("DEBUG: word = %s\n", test);
+		if (n != 3) {	/* if the group of letters is not three chars long */
+			for (n = 0; test[n+1] != '\0'; ++n)	/* 'test[n+1]' instead of 'test[n] because i don't want to print an error message for newline */
+				printf("error: 1 unknown command %c\n", test[n]);
+			return -1;
 		}
+		printf("DEBUG: got to here\n");
 		if (test[0] == 's' && test[1] == 'i' && test[2] == 'n') {
+			printf("DEBUG: returned sin\n");
 			return 0;
 		}
 		if (test[0] == 'e' && test[1] == 'x' && test[2] == 'p') {
+			printf("DEBUG: returned exp\n");
 			return 1;
 		}
 		if (test[0] == 'p' && test[1] == 'o' && test[2] == 'w') {
+			printf("DEBUG: returned pow\n");
 			return '^';
 		}
-		for (n = 0; test[n] != '\0'; ++n)
-			printf("error: not sin, exp, pow: unknown command %c\n", test[n]);
+		for (n = 0; test[n+1] != '\0'; ++n)	/* 'test[n+1]' instead of 'test[n] because i don't want to print an error message for newline */
+			printf("error: 2 unknown command %c\n", test[n]);
 	}
 	if (c == '.')		/* collect fraction part */
 		while (isdigit(s[++i] = c = getch()))
