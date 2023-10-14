093e3975522ad84237502275f61efcd321b787bf
Richard Jiang <richardjiang6@gmil.com>
2023-10-14 16:07:52 -0400

    huh. some of the stuff from oldwordle.c hasn't been uploaded to github

--- Other/wordle_stuff/oldwordle.c
+++ Other/wordle_stuff/oldwordle.c
@@ -9,8 +9,8 @@
 #define TRUE 1
 #define FALSE 0
 
-#define BLACK 0
-#define WHITE 1
+#define DEFAULT 0
+#define BLACK 1
 #define YELLOW 2
 #define GREEN 3
 
@@ -18,14 +18,15 @@ void reset();
 void setletter(char c, int status);
 int checkstatus(char c, int status);
 
-int alphabet[26];
-int green[5];
+int black[26];
+int inputstatus[5];
+int inputword[5];
 
 main()
 {
 	int i, pos, tmp, tmp2;
 	char c;
-	char word[6];
+	char word[5];
 	extern int green[5];
 	int isvalid;
 
@@ -36,12 +37,16 @@ main()
 	setletter('A', GREEN);	
 	setletter('T', GREEN);
 	setletter('E', YELLOW);
-	setletter('B', GREEN);	
-	setletter('O', GREEN);	
-	setletter('U', GREEN);	
-	setletter('S', GREEN);
+	setletter('B', BLACK);	
+	setletter('O', BLACK);	
+	setletter('U', BLACK);	
+	setletter('S', BLACK);
 	green[0] = TRUE;
 	green[4] = TRUE;
+
+	while ((c = getchar()) != EOF) {
+		if (c == '\n') {
+
 
 	/*
 	printf("Enter all letters that are black.\nCapitalization is ignored.\nEnter the number zero to stop.\n");
@@ -79,16 +84,29 @@ main()
 	}
 	*/
 
+	/* order of (ir)relevance
+	 * contains a black letter
+	 * does not contain any yellow letter
+	 * green letters do not match
+	 */
+
+	/* pseudocode
+	 * if c is a newline, check the data in word[]
+	 * if word contains a letter that is marked black,
+	 * word is not valid
+	 * if the green positions do not match
+	 * word is not valid
+	 * if any of the yellow positions match
+	 * word is not valid
+	 */
+
+	 /*
 	i = 0;
 	c = 0;
 	while ((c = getchar()) != EOF) {
 		if (c == '\n') {
-			//printf("DEBUG: c = \n");
-			i = 0;
-			continue;
-		}
-		for (tmp = 0; tmp < 5; ++tmp) {
-			if (green[tmp] == TRUE) {
+			for (tmp = 0; tmp < 5; ++tmp) {
+				if (green[tmp] == TRUE) {
 					if (!checkstatus(word[tmp], GREEN))
 						isvalid = FALSE;
 					else
@@ -136,6 +154,9 @@ main()
 				i = 0;
 				isvalid = TRUE;
 			}
+			i = 0;
+			continue;
+		} */	
 		/*
 		} else if (i == 5 && nextchar != '\n') {
 			//printf("DEBUG: skipping to the end of word\n");
@@ -145,13 +166,14 @@ main()
 			continue;
 		} */
 		//printf("DEBUG: c = %c\n", c);
-		if (isalpha(c)) {
+		//if (isalpha(c)) {
 			//printf("isalpha(c)\n");
-			word[i++] = c;
-		}
+			//word[i++] = c;
+		//}
+		/*
 		else if (c != '\n') {
 			printf("Error: %c is not a letter\n", c);
-		}
+		} */
 		/*
 		if (i == 5) {
 			printf("DEBUG: word = %s\n", word);
@@ -165,26 +187,34 @@ main()
 void reset()
 {
 	int i;
-	extern int alphabet[26];
-	extern int green[5];
+	extern int black[26];
+	extern int wordstatus[5];
+	extern int wordinput[5];
 
 	i = 0;
 	while (i < 26)
-		alphabet[i++] = WHITE;
+		black[i++] = FALSE;
 
 	i = 0;
 	while (i < 5)
-		green[i++] = FALSE;
+		wordinput[i++] = '\0';
 }
 
-void setletter(char c, int status)
+void setletter(char c, int status, int pos)
 {
 	extern int alphabet[26];
 
-	if (islower(c))
-		alphabet[c - 'a'] = status;
-	else if (isupper(c))
-		alphabet[c - 'A'] = status;
+	if (status == BLACK) {
+		if (islower(c))
+			alphabet[c - 'a'] = status;
+		else if (isupper(c))
+			alphabet[c - 'A'] = status;
+		break;
+	} else {
+		wordstatus[pos-1] = status;
+		wordinput[pos-1] = c;
+		continue;
+	}
 }
 
 int checkstatus(char c, int status)
